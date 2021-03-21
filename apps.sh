44e8eb511332e442c565382d18fd50888dd494c1
joeraven0 <joakim.ringstad.raavi@gmail.com>
2021-03-21 01:10:44 +0100

    removed imwheel, added p3x-notebook

--- apps.sh
+++ apps.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-apt install imwheel
-imwheel --kill --buttons "4 5"
+#apt install imwheel
+#imwheel --kill --buttons "4 5"
 
 apt install wipe
 apt install testdisk
 apt install clementine
+apt install npm
+npm install p3x-onenote
--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 #Users home directory /home/user
 USER_HOME=$(eval echo ~${SUDO_USER})
 
-cp imwheel.conf ~/.imwheelrc
-cp imwheel.desktop ${USER_HOME}/.config/autostart
+#cp imwheel.conf ~/.imwheelrc
+#cp imwheel.desktop ${USER_HOME}/.config/autostart
 cp bg1.jpg /usr/share/backgrounds/
 cp xfce4-desktop.xml ${USER_HOME}/.config/xfce4/xfconf/xfce-perchannel-xml
 
